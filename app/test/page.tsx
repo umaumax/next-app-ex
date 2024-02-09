d4a686813a375eebddae4ebd0f34cc0698d47ed9
umaumax <umauma7083@gmail.com>
2024-02-09 23:28:22 +0900

    Add testMessage component

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import TestMessage from './testMessage'
 
 const Home: NextPage = () => {
   return (
     <>
       <h2>test</h2>
+      <TestMessage />
     </>
   );
 };
---
+++ app/test/testMessage.tsx
@@ -0,0 +1,3 @@
+export default function TestMessage() {
+  return <div>Test message.</div>
+}
