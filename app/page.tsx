b5af98c31a5748a0fee680962ee312148bd47c3b
umaumax <umauma7083@gmail.com>
2024-02-09 23:30:53 +0900

    Add test page link

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <Link href="test">Test</Link>
+    </>
+  )
 }
